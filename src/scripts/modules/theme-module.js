48eb9fe92bbfd450d8709686c7225bc146fa2ebd
jay-fullstack <80300747+jay-fullstack@users.noreply.github.com>
2021-04-15 14:08:26 +0800

    Adding updated sleep page and section reusable on my branch. (#44)
    * homepage-section-desktop
    
    * Fixed issue
    
    * header-section help products (#17)
    
    * header-section help products
    
    * fixed-issues
    
    * footer - work in progress (#18)
    
    * footer - work in progress
    
    * footer -  issues  fixed
    
    * fixed-issues
    
    * fix buttons
    
    * wip- footer issues
    
    * footer done
    
    * header & section how it works (done), section ratings (wip) (#21)
    
    * header-section help products
    
    * fixed-issues
    
    * header-resubale/section-testimonial-reusable
    
    * section-ratings
    
    * fixed-header-js
    
    * Footer and questions answered (#20)
    
    * footer - work in progress
    
    * footer -  issues  fixed
    
    * fix buttons
    
    * wip- footer issues
    
    * footer done
    
    * section questions answered done
    
    * fixed questions answered issues
    
    * fixed-issues
    
    * fixed-issues
    
    * done-section-treatment
    
    * fixed-issues
    
    * Done Desktop - section How it works, Expert care, Sleep, Axiety
    
    * Section Personalized Mobile - done
    
    * mobile version - section Personalized, quotation slider, sleep, anxiety, how it works
    
    * mobile version - section Personalized, quotation slider, sleep, anxiety, how it works
    
    * fixed issue
    
    * fixed issued
    
    * sleep page - section, hero, why trials, trials work, sleep medications [ desktop ]
    
    * sleep page - section doctors word done [desktop]
    
    * Sleep page - section why trials, doctors word, incredibly personal, science sleep. Desktop done
    
    * sleep page - mobile view done
    
    * adding typhography.scss
    
    * Adding sleep page
    
    * Adding sleep.scss  page
    
    * update section incredibly-personal
    
    * update section science-sleep
    
    * adding section axiety solution [reusable]
    
    * adding section sleep solution [reusable]
    
    * updated main.scss
    
    * updated index.scss
    
    * fixed font should be typhopography.scss on _home.scss
    
    * fixed nav-link duplicate
    
    * _header.scss fixed duplicate class
    
    * _header.scss fixed duplicate class v2
    
    Co-authored-by: guintojay <jhayguinto07@gmail.com>
    Co-authored-by: nadia-fullstack <79129155+nadia-fullstack@users.noreply.github.com>
    Co-authored-by: Adi Lila Dasi Robles <79141549+AdiRobles@users.noreply.github.com>

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -13,7 +13,16 @@ AppName.Modules.ThemeModule = (function () {
         el: '.swiper-pagination',
       },
     });
+    
+    $(window).on("scroll", function() {
+      if($(window).scrollTop() > 50) {
+          $(".scroll-active-js").addClass("active");
+      } else {
+         $(".scroll-active-js").removeClass("active");
+      }
+  });
   };
+
 
   /////////////////////
   // Public Methods //
